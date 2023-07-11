79437df91917e352a777c00859778da3eca0a66f
Trdat Mkrtchyan <wombtromb@gmail.com>
2023-07-11 16:08:31 +0400

    fix: updated data refs notations (#90)
    * fix: updated data refs notations
    * fix(data-refs): fixed sum of integrals on scale

--- tests/plugins/dataRefs.test.ts
+++ tests/plugins/dataRefs.test.ts
@@ -6,7 +6,8 @@ describe('DataRefs plugin', () => {
         timeline: [1, 2, 3, 4],
         series: [
             {data: [1, 2, 3, 4], scale: 'y', id: 'one'},
-            {data: [5, 6, 7, 8], scale: 'r'},
+            {data: [3, 3, 3, 3], scale: 'y', id: 'two'},
+            {data: [5, 6, 7, 8], scale: 'r', id: 'three'},
         ],
         plugins: {
             refs: DataRefs({}),
@@ -16,8 +17,8 @@ describe('DataRefs plugin', () => {
     it('should calc refs for series perScale', async () => {
         await new Promise((resolve) => setTimeout(resolve, 100));
         expect(y.plugins.refs?.getRefs()).toEqual({
-            y: {min: 1, max: 4, count: 4, avg: 2.5, sum: 10, integral: 0.0075},
-            r: {min: 5, max: 8, count: 4, avg: 6.5, sum: 26, integral: 0.0195},
+            y: {min: 1, max: 4, count: 8, last: null, avg: 2.75, sum: 22, integral: 0.0165},
+            r: {min: 5, max: 8, count: 4, last: null, avg: 6.5, sum: 26, integral: 0.0195},
         });
     });
 
@@ -28,8 +29,68 @@ describe('DataRefs plugin', () => {
             max: 2,
             count: 2,
             avg: 1.5,
+            last: 2,
             sum: 3,
             integral: 0.0015,
+        });
+    });
+
+    it('should calc refs by ranges', async () => {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        expect(y.plugins.refs?.getRefs(0, 1)).toEqual({
+            y: {
+                series: {
+                    one: {
+                        min: 1,
+                        max: 2,
+                        count: 2,
+                        avg: 1.5,
+                        sum: 3,
+                        integral: 0.0015,
+                        last: 2,
+                    },
+                    two: {
+                        avg: 3,
+                        count: 2,
+                        integral: 0.003,
+                        last: 3,
+                        max: 3,
+                        min: 3,
+                        sum: 6,
+                    },
+                },
+                total: {
+                    avg: 2.25,
+                    count: 4,
+                    integral: 0.0045000000000000005,
+                    last: null,
+                    max: 3,
+                    min: 1,
+                    sum: 9,
+                },
+            },
+            r: {
+                series: {
+                    three: {
+                        avg: 5.5,
+                        count: 2,
+                        integral: 0.0055,
+                        last: 6,
+                        max: 6,
+                        min: 5,
+                        sum: 11,
+                    },
+                },
+                total: {
+                    avg: 5.5,
+                    count: 2,
+                    integral: 0.0055,
+                    last: null,
+                    max: 6,
+                    min: 5,
+                    sum: 11,
+                },
+            },
         });
     });
 });
