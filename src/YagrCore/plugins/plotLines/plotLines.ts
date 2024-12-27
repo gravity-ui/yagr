eaf55952845f2524e4334d7c37fd897dbeb9a857
Anastasiia Beriia <estasieberia@yandex-team.ru>
2024-12-27 15:50:59 +0400

    fix: id generation

--- src/YagrCore/plugins/plotLines/plotLines.ts
+++ src/YagrCore/plugins/plotLines/plotLines.ts
@@ -6,6 +6,7 @@ import {DrawOrderKey} from '../../utils/types';
 import {PBandConfig} from 'src/types';
 import {deepIsEqual} from '../../utils/common';
 import {calculateFromTo} from './utils';
+import {v4 as uuid} from 'uuid';
 
 const MAX_X_SCALE_LINE_OFFSET = 0;
 const DRAW_MAP = {
@@ -48,14 +49,13 @@ export default function plotLinesPlugin(options: PlotLineOptions): PlotLinesPlug
         const drawIndicies = (drawOrder ? drawOrder.map((key) => DRAW_MAP[key]) : [0, 1, 2]).join('');
 
         const hook = HOOKS_MAP[drawIndicies] || 'drawClear';
-        let lineCounter = 0;
 
         function getLineId(line: PlotLineConfig): string {
             if (line.id) {
                 return line.id;
             }
             const lineWithoutId = Array.from(plotLines.entries()).find(([_, l]) => deepIsEqual(l, line))?.[0];
-            return lineWithoutId || `plot-line-${++lineCounter}`;
+            return lineWithoutId || uuid();
         }
 
         function renderPlotLines(u: UPlot) {
@@ -200,13 +200,13 @@ export default function plotLinesPlugin(options: PlotLineOptions): PlotLinesPlug
                 opts: () => {
                     const config = yagr.config;
                     plotLines = new Map();
-                    let counter = 0;
+
                     for (const scale in config.axes) {
                         if (config.axes.hasOwnProperty(scale)) {
                             const axisConfig = config.axes[scale];
                             if (axisConfig.plotLines) {
                                 for (const plotLine of axisConfig.plotLines) {
-                                    plotLines.set(plotLine.id || `plot-line-${++counter}`, {...plotLine, scale});
+                                    plotLines.set(plotLine.id || uuid(), {...plotLine, scale});
                                 }
                             }
                         }
